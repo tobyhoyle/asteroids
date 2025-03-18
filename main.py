c2a81052353357ffe7c1615363c1c2eeb6148b97
Toby Hoyle <toby.hoyle@gmail.com>
2025-03-18 11:23:28 -0400

    feat: added player

--- main.py
+++ main.py
@@ -2,20 +2,27 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
